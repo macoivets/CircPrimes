7681ee70b5483f32508ddef0890fa5505eba754d
macoivets <macoivets@yandex.ru>
2016-09-05 19:13:37 +0300

    add Prime

--- CircPrimes2/Program.cs
+++ CircPrimes2/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CircPrimes2
+namespace CircPrimes
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int limit = 1000000;
+            List<int> Primes = new List<int>();
+            Primes = Prime.findPrimes(limit);
+            Primes = Prime.findCircularPrimes(Primes);
+            Console.WriteLine("The number of ciruclar primes below {0} is {1}", limit, Primes.Count);
+
+            Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 0; i < Primes.Count; i++)
+            {
+                Console.WriteLine("{0}", Primes[i]);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
